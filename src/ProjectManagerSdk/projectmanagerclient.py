60270cb9461004891220d7dd08988cbc1ff37a3e
tspence <1933486+tspence@users.noreply.github.com>
2026-05-31 00:59:52 +0000

    ## Patch notes for 2026.156.163
    These patch notes summarize the changes from version 2026.156.162.
    
    Changes to existing endpoints:
    * Breaking change: TaskField.RetrieveTaskFieldValue path changed from `/api/data/tasks/{taskId}/fields/{fieldId}` to `/api/data/tasks/{taskId}/fields/{fieldId}/values`
    * Breaking change: TaskField.UpdateTaskFieldValue path changed from `/api/data/tasks/{taskId}/fields/{fieldId}` to `/api/data/tasks/{taskId}/fields/{fieldId}/values`
    * Breaking change: TaskStatus.UpdateTaskStatus path changed from `/api/data/tasks/statuses/{taskStatusId}` to `/api/data/projects/{projectId}/tasks/statuses`
    * TaskStatus.UpdateTaskStatus added path parameter `projectId`
    * TaskStatus.UpdateTaskStatus removed path parameter `taskStatusId`
    * Breaking change: TaskStatus.DeleteTaskStatus path changed from `/api/data/projects/{projectId}/tasks/statuses/{taskStatusId}` to `/api/data/tasks/statuses/{taskStatusId}`
    * TaskStatus.DeleteTaskStatus removed path parameter `projectId`

--- src/ProjectManagerSdk/projectmanagerclient.py
+++ src/ProjectManagerSdk/projectmanagerclient.py
@@ -9,7 +9,7 @@
 # @author     ProjectManager.com <support@projectmanager.com>
 #             
 # @copyright  2023-2026 ProjectManager.com, Inc.
-# @version    2026.156.162
+# @version    2026.156.163
 # @link       https://github.com/projectmgr/projectmanager-sdk-python
 #
 
@@ -157,7 +157,7 @@ class ProjectManagerClient:
         if env == "production":
             self.serverUrl = "https://api.projectmanager.com"
         self.sdkName = "Python"
-        self.sdkVersion = "2026.156.162"
+        self.sdkVersion = "2026.156.163"
         self.machineName = platform.uname().node
         self.applicationName = appname
         self.bearerToken = None
